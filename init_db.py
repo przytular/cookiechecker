7001cc07ffc25d269a023c5846a4553d7e6e597d
Radosław Przytuła <radoslaw.przytula@gmail.com>
2019-05-08 17:00:14 +0200

    improve db structure, set root login

--- init_db.py
+++ init_db.py
@@ -8,13 +8,16 @@ table = schema.Table('credentials', metadata,
     schema.Column('id', types.Integer, primary_key=True),
     schema.Column('login', types.Unicode(255), default=u''),
     schema.Column('password', types.Unicode(255), default=u''),
+    schema.Column('cookie_name', types.Unicode(255), default=u''),
     schema.Column('cookie_content', types.Unicode(255), default=u''),
+    schema.Column('ASPNET', types.Boolean(), default=False),
+    schema.Column('url', types.Unicode(255), default=u''),
     schema.Column('last_updated', types.DateTime, server_default=text('NOW()'))
 )
 
 ### CONNECT TO DB
 engine = create_engine('mysql+mysqldb://{}:{}@{}:{}/{}'.format(
-	CONFIG['DB_USER'], CONFIG['DB_PASS'], CONFIG['DB_HOST'], CONFIG['DB_PORT'], CONFIG['DB_NAME']))
+	'root', 'root', CONFIG['DB_HOST'], CONFIG['DB_PORT'], CONFIG['DB_NAME']))
 metadata.bind = engine
 
 ### CREATE TABLES
